61a15db2cf5440d827788255005848d79259d70b
Christophe Diericx <christophe@diericx.net>
2016-10-01 21:55:55 -0300

    Adapt build.bat.

--- Source/build.bat
+++ Source/build.bat
@@ -14,11 +14,18 @@ if "%nuget%" == "" (
 	set nuget=nuget
 )
 
+echo -- Build ArduinoLibCSharp...
+
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Source/ArduinoLibCSharp.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
 mkdir Build
 mkdir Build\lib
 mkdir Build\lib\net40
 
+echo -- Packaging...
+
+echo -- Package ArduinoUploader...
 %nuget% pack "Source\ArduinoLibCSharp.ArduinoUploader\ArduinoLibCSharp.ArduinoUploader.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+
+echo -- Package IntelHexFormatReader...
 %nuget% pack "Source\ArduinoLibCSharp.IntelHexFormatReader\ArduinoLibCSharp.IntelHexFormatReader.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
